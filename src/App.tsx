afc6cd0b12ae5749c7dbf8267bc6c6bf26a33ce8
caarlosdamian <dapecarlos@gmail.com>
2022-07-23 14:21:38 -0500

    added top section with responsive desing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import background from "./images/bg-pattern.svg";
 import "./App.scss";
+import { Top } from "./sections/top/Top";
+import { Card } from "./components/card/Card";
 
 function App() {
   return (
     <main className="main-container">
+      <Top/>
       <img src={background} alt="background" className="background-photo" />
+      <Card/>
     </main>
   );
 }
--- src/sections/top/Top.tsx
+++ src/sections/top/Top.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import patter from '../../images/pattern-circles.svg'
+import './Top.scss';
 
 export const Top = () => {
   return (
-    <div>Top</div>
-  )
-}
+    <div className="top-container">
+      <h1 className="top-header">Simple, traffic-based pricing</h1>
+      <h3 className="top-subheader">
+        Sign-up for our 30-day trial. No credit card required.{" "}
+      </h3>
+      <img src={patter} alt="patter"  className="patter-img"/>
+    </div>
+  );
+};
